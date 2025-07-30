3a66dd3930ae55664a9171f735597647b6dcb071
Eduardo Conte <eduardo.conte.dev@gmail.com>
2025-07-30 18:10:16 -0300

    Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export default function Header() {
+  return (
+    <header>
+      <div className="ct-container teste">
+        <h1>Carros Top</h1>
+      </div>
+    </header>
+  )
+}
